4e801ade5965d98efa8fbc143fecfa4d97dc7ae3
Nicolas Garcia Brogna <ngarciab92@gmail.com>
2023-11-03 12:56:39 -0300

    Final build
    love calculator avoid that my crush get mad at me

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function calculateLove() {
+    const name1 = document.getElementById("name1").value.trim();
+    const name2 = document.getElementById("name2").value.trim();
+    if (name1 === "" || name2 === "") {
+        alert("Plase enter both names.");
+    } else {
+
+        let lovePercentage = Math.floor(Math.random() * 101);
+        const result = document.getElementById("result");
+        if ((name1 === "camila" && name2 === "nicolas") || (name1 === "nicolas" && name2 === "camila")) {
+            lovePercentage = 100;
+        }
+        console.log(lovePercentage);
+
+        result.innerHTML = `<span class="nameColor">${name1} </span> and <span class="nameColor">${name2} 's </span>  Love porcentage: ${lovePercentage} %`;
+
+        if (lovePercentage < 30) {
+            result.innerHTML += "<br> Not a Great Match. Sorry!"
+
+        } else if (lovePercentage >= 30 && lovePercentage < 70) {
+            result.innerHTML += "<br> There is potential. Give it a try !"
+        } else if (lovePercentage >= 30 && lovePercentage <= 99) {
+            result.innerHTML += "<br> Great Match!!! You could be happy togheter ☻  "
+
+        } else {
+            result.innerHTML += "<br> ♥ Perfect Match!! Love is in the Air! ♥"
+        }
+    }
+}
